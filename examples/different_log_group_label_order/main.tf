43ecbdcd336d5843140526af83e85cf197d0f933
Sebastian Struß <31846129+applike-ss@users.noreply.github.com>
2022-12-08 13:43:17 +0100

    feat: Added module functionality (#1)
    ## Description
    <!--- Describe your changes in detail -->
    
    ## Motivation and Context
    <!--- Why is this change required? What problem does it solve? -->
    <!--- If it fixes an open issue, please link to the issue here. -->
    
    ## Breaking Changes
    <!-- Does this break backwards compatibility with the current major
    version? -->
    <!-- If so, please provide an explanation why it is necessary. -->
    
    ## How Has This Been Tested?
    - [ ] I have updated at least one of the `examples/*` to demonstrate and
    validate my change(s)
    - [ ] I have tested and validated these changes using one or more of the
    provided `examples/*` projects
    <!--- Users should start with an existing example as its written, deploy
    it, then check their changes against it -->
    <!--- This will highlight breaking/disruptive changes. Once you have
    checked, deploy your changes to verify -->
    <!--- Please describe how you tested your changes -->
    - [ ] I have executed `pre-commit run -a` on my pull request
    <!--- Please see
    https://github.com/antonbabenko/pre-commit-terraform#how-to-install for
    how to install -->

---
+++ examples/different_log_group_label_order/main.tf
@@ -0,0 +1,12 @@
+module "example" {
+  source = "../.."
+
+  label_order                        = ["name", "attributes"]
+  log_group_label_order              = ["environment", "name"]
+  name                               = "tracing"
+  region                             = "eu-central-1"
+  stage                              = "stage"
+  environment                        = "env"
+  ecs_cluster_arn                    = "arn:aws:ecs:eu-central-1:123456789012:cluster/my-cluster"
+  service_discovery_dns_namespace_id = "ns-12345678"
+}
---
+++ main.tf
@@ -0,0 +1,94 @@
+locals {
+  xray_service_placement_constraints = module.this.environment == "prod" ? [{
+    type       = "memberOf"
+    expression = "attribute:spotinst.io/container-instance-lifecycle==od"
+  }] : []
+}
+
+module "log_group_label" {
+  source  = "cloudposse/label/null"
+  version = "0.25.0"
+
+  context = module.this.context
+
+  label_order = var.log_group_label_order
+}
+
+resource "aws_cloudwatch_log_group" "xray" {
+  name              = "${module.log_group_label.id}-xray"
+  tags              = module.log_group_label.tags
+  retention_in_days = var.log_retention_in_days
+}
+
+resource "aws_service_discovery_service" "xray" {
+  name = "xray"
+
+  dns_config {
+    namespace_id = var.service_discovery_dns_namespace_id
+
+    dns_records {
+      ttl  = 60
+      type = "SRV"
+    }
+  }
+
+  health_check_custom_config {
+    failure_threshold = 1
+  }
+
+  tags = module.this.tags
+}
+
+module "xray_definition" {
+  source  = "cloudposse/ecs-container-definition/aws"
+  version = "0.58.1"
+
+  container_image              = "public.ecr.aws/xray/aws-xray-daemon:3.3.5"
+  container_name               = "xray-daemon"
+  container_cpu                = module.this.environment == "prod" ? 100 : 25
+  container_memory_reservation = module.this.environment == "prod" ? 100 : 50
+
+  port_mappings = [
+    {
+      containerPort = 2000
+      hostPort      = 0
+      protocol      = "udp"
+    },
+  ]
+
+  log_configuration = {
+    logDriver = "awslogs"
+    options = {
+      awslogs-group         = aws_cloudwatch_log_group.xray.name
+      awslogs-region        = var.region
+      awslogs-stream-prefix = "ecs"
+    }
+  }
+}
+
+module "ecs_xray_task" {
+  source  = "cloudposse/ecs-alb-service-task/aws"
+  version = "0.64.1"
+
+  task_policy_arns          = ["arn:aws:iam::aws:policy/AWSXRayDaemonWriteAccess"]
+  network_mode              = var.network_mode
+  container_definition_json = "[${module.xray_definition.json_map_encoded}]"
+  scheduling_strategy       = "REPLICA"
+  service_registries = [{
+    registry_arn   = aws_service_discovery_service.xray.arn
+    container_name = "xray-daemon"
+    container_port = 2000
+  }]
+  ecs_cluster_arn                    = var.ecs_cluster_arn
+  launch_type                        = "EC2"
+  deployment_minimum_healthy_percent = 0
+  deployment_maximum_percent         = 100
+  desired_count                      = 1
+  vpc_id                             = var.vpc_id
+  propagate_tags                     = "SERVICE"
+  context                            = module.this.context
+  service_placement_constraints      = local.xray_service_placement_constraints
+  tags = {
+    "spotinst.io/restrict-scale-down" = true
+  }
+}
